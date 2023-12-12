b35707fc008be8a3bb419822ff3fa1a48d5dcaff
Christopher Jin <chrisjin1@gmail.com>
2023-12-12 17:42:26 -0500

    flexing the things container

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const saveInd = document.getElementById('saveIndex')
 
 let todoArray = []
 displayTodo()
+
 function displayTodo() {
   let todo = localStorage.getItem('todo')
   if (todo === null) {
@@ -13,42 +14,25 @@ function displayTodo() {
   } else {
     todoArray = JSON.parse(todo)
   }
+
   let htmlCode = ''
   todoArray.forEach((list, ind) => {
     htmlCode += `<div class='flex mb-4 items-center'>
-      <p class='w-full text-grey-darkest'>${list}</p>
-      <button onclick='edit(${ind})' class='flex-no-shrink p-2 ml-4 mr-2 border-2 rounded text-white text-grey bg-green-600'>Edit</button>
-      <button onclick='deleteTodo(${ind})' class='flex-no-shrink p-2 ml-2 border-2 rounded text-white bg-red-500'>Delete</button>
-      <div class="flex items-center mb-4">
-        <div class='flex flex-col items-center'>
-            <label for="monday${ind}" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Monday</label>
-            <input id="monday${ind}" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-        </div>
-        <div class='flex flex-col items-center'>
-            <label for="tuesday${ind}" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Tuesday</label>
-            <input id="tuesday${ind}" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-        </div>
-        <div class='flex flex-col items-center'>
-            <label for="wednesday${ind}" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Wednesday</label>
-            <input id="wednesday${ind}" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-        </div>
-        <div class='flex flex-col items-center'>
-            <label for="thursday${ind}" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Thursday</label>
-            <input id="thursday${ind}" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-        </div>
-        <div class='flex flex-col items-center'>
-            <label for="friday${ind}" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Friday</label>
-            <input id="friday${ind}" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-        </div>
-        <div class='flex flex-col items-center'>
-            <label for="saturday${ind}" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Saturday</label>
-            <input id="saturday${ind}" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-        </div>
-        <div class='flex flex-col items-center'>
-            <label for="sunday${ind}" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Sunday</label>
-            <input id="sunday${ind}" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-        </div>
-      </div>
+    <div class='w-1/3 border border-gray-900 flex'>
+        <p class='w-full text-grey-darkest'>${list}</p>
+        <button onclick='edit(${ind})' class='border border-green-500 h-8 w-8'>Edit</button>
+        <button onclick='deleteTodo(${ind})' class='border border-red-500 h-8 w-8'>X</button>
+    </div>
+
+    <div class='w-2/3'><input id="monday${ind}" type="checkbox" value="" class="h-8 w-8 m-1 rounded-full">
+        <input id="tuesday${ind}" type="checkbox" value="" class="h-8 w-8 m-1 rounded-full">
+        <input id="wednesday${ind}" type="checkbox" value="" class="h-8 w-8 m-1 rounded-full">
+        <input id="thursday${ind}" type="checkbox" value="" class="h-8 w-8 m-1 rounded-full">
+        <input id="friday${ind}" type="checkbox" value="" class="h-8 w-8 m-1 rounded-full">
+        <input id="saturday${ind}" type="checkbox" value="" class="h-8 w-8 m-1 rounded-full">
+        <input id="sunday${ind}" type="checkbox" value="" class="h-8 w-8 m-1 rounded-full">
+    </div>    
+      
    </div>`
   })
   listBox.innerHTML = htmlCode
@@ -96,3 +80,19 @@ saveTaskButton.addEventListener('click', () => {
   localStorage.setItem('todo', JSON.stringify(todoArray))
   displayTodo()
 })
+
+/* -- Glow effect -- */
+
+const blob = document.getElementById('blob')
+
+window.onpointermove = (event) => {
+  const { clientX, clientY } = event
+
+  blob.animate(
+    {
+      left: `${clientX}px`,
+      top: `${clientY}px`,
+    },
+    { duration: 3000, fill: 'forwards' }
+  )
+}
